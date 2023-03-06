515a3137310201a5dd79546c37d1a5816510d0c1
Sean Stidham <seanstidham14@gmail.com>
2023-03-06 13:32:11 -0700

    login understanding

--- newsimon/login.js
+++ newsimon/login.js
@@ -3,3 +3,4 @@ function login() {
   localStorage.setItem("userName", nameEl.value);
   window.location.href = "play.html";
 }
+//login function puts the value of name to the input then stores it in the local storage as userName then redirects the ser to play.html
